60e22cf6c1ebae0985b7ff827c861c9db54ed8a3
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-04-05 22:36:54 +0200

    Initial Commit

---
+++ Apple 3D Earth/Apple_3D_EarthApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_3D_EarthApp.swift
+//  Apple 3D Earth
+//
+//  Created by Damien Gautier on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Apple_3D_EarthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
